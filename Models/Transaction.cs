ed95a27a9476c2b4ad771460aa3d340fffcc3d86
ShanOffice <shan@office>
2024-08-09 08:45:30 +0500

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker_WebApp.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
